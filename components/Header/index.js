688a23448a536447abbafa205abffadff6e7defa
codeasaglacier <remingtonshock@gmail.com>
2019-10-11 15:57:13 -0800

    created header

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,30 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+const headerContainer = document.querySelector('.header-container')
 
-function Header() {}
+headerContainer.appendChild(Header())
+
+function Header() {
+    const headerBox = document.createElement('div')
+    const dateSpan = document.createElement('span')
+    const title = document.createElement('h1')
+    const tempSpan = document.createElement('span')
+    
+    headerBox.appendChild(dateSpan)
+    headerBox.appendChild(title)
+    headerBox.appendChild(tempSpan)
+
+    headerBox.classList.add('header')
+    dateSpan.classList.add('date')
+    tempSpan.classList.add('temp')
+
+    dateSpan.textContent = 'SMARCH 28, 2019'
+    title.textContent = 'Lambda Times'
+    tempSpan.textContent = '98°'
+
+    console.log(headerBox)
+    
+    
+    return headerBox
+}
